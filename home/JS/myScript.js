516898c8b3b5c2f4770e013a213fa1e6847be07e
KurtLeonardSauquillo <152581258+KurtLeonardSauquillo@users.noreply.github.com>
2024-12-19 09:55:36 +0800

    added try it

--- home/JS/myScript.js
+++ home/JS/myScript.js
@@ -1,3 +1,10 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
+// myScript.js
+ 
+// Function 1 - Changes the text of the paragraph to "Function 1 triggered."
+function function1() {
+  document.getElementById("demo").innerHTML = "Function 1 triggered.";
+}
+ 
+// Function 2 - Changes the text of the paragraph to "Function 2 triggered."
+function function2() {
+  document.getElementById("demo").innerHTML = "Function 2 triggered.";
