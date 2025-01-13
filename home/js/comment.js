552611629414f3adef9c6652ddcd55bf70aed9ac
KurtLeonardSauquillo <152581258+KurtLeonardSauquillo@users.noreply.github.com>
2025-01-13 19:38:41 +0800

    comment

--- home/js/comment.js
+++ home/js/comment.js
@@ -7,13 +7,18 @@ new Vue({
     },
     methods: {
       submitComment() {
+        // Check if both the name and the comment text are filled
         if (this.name && this.commentText) {
+          // Add the comment to the comments array
           this.comments.push({
             name: this.name,
             text: this.commentText
           });
+          // Clear the input fields after submission
           this.name = '';
           this.commentText = '';
+        } else {
+          alert('Please provide both name and comment!');
         }
       }
     }
