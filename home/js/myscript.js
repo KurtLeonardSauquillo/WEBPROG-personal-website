0dce1e772217512f1bc4db0738d3516ef1917ce9
KurtLeonardSauquillo <152581258+KurtLeonardSauquillo@users.noreply.github.com>
2025-01-13 20:48:40 +0800

    tapos na

---
+++ home/js/myscript.js
@@ -0,0 +1,21 @@
+function toggleSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.style.display = section.style.display === 'none' ? 'block' : 'none';
+  }
+  
+  new Vue({
+    el: '#app',
+    data: {
+      newComment: '',
+      comments: []
+    },
+    methods: {
+      addComment() {
+        if (this.newComment.trim() !== '') {
+          this.comments.push(this.newComment);
+          this.newComment = '';
+        }
+      }
+    }
+  });
+  
