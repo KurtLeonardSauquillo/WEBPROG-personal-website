3f4c6f2bbd0fc7f9ddae58bda3df748184c9a821
KurtLeonardSauquillo <152581258+KurtLeonardSauquillo@users.noreply.github.com>
2025-03-10 14:27:51 +0800

    update

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -1,21 +1,20 @@
 function toggleSection(sectionId) {
-    const section = document.getElementById(sectionId);
-    section.style.display = section.style.display === 'none' ? 'block' : 'none';
-  }
-  
-  new Vue({
-    el: '#app',
-    data: {
+  const section = document.getElementById(sectionId);
+  section.style.display = section.style.display === 'none' ? 'block' : 'none';
+}
+
+new Vue({
+  el: '#app',
+  data: {
       newComment: '',
       comments: []
-    },
-    methods: {
+  },
+  methods: {
       addComment() {
-        if (this.newComment.trim() !== '') {
-          this.comments.push(this.newComment);
-          this.newComment = '';
-        }
+          if (this.newComment.trim() !== '') {
+              this.comments.push(this.newComment);
+              this.newComment = '';
+          }
       }
-    }
-  });
-  
+  }
+});
